bc561ef3d35937880e1bcd47f8ca97a3a3c84181
edisonskiploop <edison@skiploop.com>
2016-11-25 21:04:33 +0530

    Pseudo Classes Added

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 angular.module('chips-input', [])
     .controller('ctrl', function ($scope, $window) {
         $scope.event = '';
-        // $scope.inputSize = 2;
         $scope.chipName = '';
-        $scope.maxlength = 15;
-        $scope.chips = [];
+        $scope.maxlength = 115;
+        $scope.chips = ['Hey Buddy!'];
 
         $scope.process = function (event) {
             if (event.keyCode == 32 || event.keyCode == 13) {
                 if ($scope.chipName != "") {
                     $scope.chips.push($scope.chipName);
                     $scope.chipName = '';
-                    // $scope.inputSize = 2;
-                    // $window.document.getElementById('input-chip').style.width = 5 + 'px';
                 }
             }
 
@@ -21,9 +18,6 @@ angular.module('chips-input', [])
                     $scope.chips.pop();
             }
 
-            // if ($scope.inputSize < 45)
-            //     $scope.inputSize++;
-            // $window.document.getElementById('input-chip').style.width = $scope.inputSize * 5 + 'px';
             $window.document.getElementById('input-chip').focus();
         };
 
@@ -71,6 +65,7 @@ angular.module('chips-input', [])
         $scope.loadInputStyles = function () {
             return {
                 'height': '48px',
+                'min-width' : '15px',
                 'display': 'inline-block',
                 'font-size': '18px',
                 'line-height': '50px',
@@ -82,8 +77,17 @@ angular.module('chips-input', [])
                 'color': '#cacaca'
             };
         };
+        
+        var css = '.chips-input:focus {outline: none;} .removeChip:hover {color : #5f5f5f !important;}',
+            head = document.head || document.getElementsByTagName('head')[0],
+            style = document.createElement('style');
 
-        // var style = "<style>.abc {color:#fff;}";
-        // document.getElementsByTagName('head').append(style);
+        style.type = 'text/css';
+        if (style.styleSheet){
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
 
+        head.appendChild(style);
     });
