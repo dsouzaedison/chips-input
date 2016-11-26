1e1a09bb426de5265bf174c5c66f6a7d8df0cf17
edisonskiploop <edison@skiploop.com>
2016-11-26 23:39:18 +0530

    Project Cleanup

--- demo/chips.js
+++ demo/chips.js
@@ -1,5 +1,5 @@
 angular.module('myModule', ['chips-input'])
     .controller('myCtrl', function ($scope, chipsInput) {
         // chipsInput.enableAutofocus();
-        chipsInput.init(["hi", "billy"]);
+        chipsInput.init(["hey", "billy"]);
     });
